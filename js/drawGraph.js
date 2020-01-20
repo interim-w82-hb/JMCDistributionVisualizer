9907d0175642d7c82fcb9bcd101808d0415ebdda
beg23 <beg23@maroon08.cs.calvin.edu>
2020-01-20 11:49:10 -0500

    Complete cdf functionality and begin 3d

--- js/drawGraph.js
+++ js/drawGraph.js
@@ -1,82 +1,54 @@
-//Create test data
-let testData = [];
-for(let i = 0; i < 100; i++){
-    testData[i] = {x: i, y: Math.pow((.1*i), 2)};
+// function to graph a given set of points
+function createPlot(data, plotSvg, xAxisUnits, yAxisUnits, width, height) {
+
+  // add the x Axis
+  const x = d3.scaleLinear()
+              .domain(d3.extent(data.map(d => d[xAxisUnits])))
+              .range([0, width]);
+  plotSvg.append('g')
+         .attr('transform', 'translate(0,' + height + ')')
+         .call(d3.axisBottom(x));
+
+  // add the y Axis
+  const y = d3.scaleLinear()
+              .domain(d3.extent(data.map(d => d[yAxisUnits])))
+              .range([height, 0]);
+  plotSvg.append('g')
+         .call(d3.axisLeft(y));
+
+  // plot the curve
+  plotSvg.append('path')
+         .attr('class', 'mypath')
+         .datum(data)
+         .attr('fill', 'red')
+         .attr('opacity', '.8')
+         .attr('stroke', '#000')
+         .attr('stroke-width', 1)
+         .attr('stroke-linejoin', 'round')
+         .attr('d', d3.line()
+                      .curve(d3.curveBasis)
+                      .x(function(d) { return x(d[xAxisUnits]); })
+                      .y(function(d) { return y(d[yAxisUnits]); })
+         );
+
+  // add endpoints so fill is even
+  let oldPath = plotSvg.select('.mypath').attr('d');
+  let newPath = oldPath + 'L' + width + ',' + height + 'L' + 0 + ',' + height
+  plotSvg.select('.mypath')
+         .attr('d', newPath);
 }
 
-//Initialize graphic dimensions
-const graphicHeight = 400;
-const graphicWidth = 460;
-
-// set the dimensions and margins of the plot
-const margin = {top: 30, right: 30, bottom: 30, left: 50};
-const plotWidth = graphicWidth - margin.left - margin.right;
-const plotHeight = graphicHeight - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var plotSvg = d3.select("#graphic")
-    .append("svg")
-    .attr("width", plotWidth + margin.left + margin.right)
-    .attr("height", plotHeight + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-graphDensityPlot(testData, plotSvg, 'x', 'y', plotWidth, plotHeight);
-
-
-// get the data
-function graphDensityPlot(data, plotSvg, xAxisUnits, yAxisUnits, width, height) {
-    // add the x Axis
-    const x = d3.scaleLinear()
-        .domain(d3.extent(data.map(d => d[xAxisUnits])))
-        .range([0, width]);
-    plotSvg.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
-    // add the y Axis
-    const y = d3.scaleLinear()
-        .domain([0, 0.1])
-        .range([height, 0]);
-    plotSvg.append("g")
-        .call(d3.axisLeft(y)); 
-
-        // Compute kernel density estimation
-    const density = kde(kernelEpanechnikov(7), x.ticks(100))
-        ( data.map(function(d){  return d[yAxisUnits]; }) )
-
-        // Plot the area
-    plotSvg.append("path")
-        .attr("class", "mypath")
-        .datum(density)
-        .attr("fill", "#69b3a2")
-        .attr("opacity", ".8")
-        .attr("stroke", "#000")
-        .attr("stroke-width", 1)
-        .attr("stroke-linejoin", "round")
-        .attr("d",  d3.line()
-        .curve(d3.curveBasis)
-            .x(function(d) { return x(d[0]); })
-            .y(function(d) { return y(d[1]); })
-        );
-    
-    //Add endpoints so fill is even
-    let oldPath = plotSvg.select(".mypath").attr('d');
-    let newPath = oldPath + 'L'+width+','+height+'L'+0+','+height
-    plotSvg.select('.mypath')
-        .attr('d', newPath);
-}
-
-// Function to compute density
+// functions to compute density (TODO: necessary?)
 function kde(kernel, X) {
-  return function(V) {
-    return X.map(function(x) {
-      return [x, d3.mean(V, function(v) { return kernel(x - v); })];
-    });
-  };
+    return function(V) {
+        return X.map(function(x) {
+            return [x, d3.mean(V, function(v) { return kernel(x - v); })];
+        });
+    };
 }
+
 function kernelEpanechnikov(k) {
-  return function(v) {
-    return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
-  };
+    return function(v) {
+        return Math.abs(v /= k) <= 1 ? 0.75 * (1 - v * v) / k : 0;
+    };
 }
--- js/gatherData.js
+++ js/gatherData.js
@@ -1,23 +1,46 @@
+// initialize graphic dimensions
+const graphicHeight = 400;
+const graphicWidth = 460;
+
+// set the dimensions and margins of the plot
+const margin = {top: 30, right: 30, bottom: 30, left: 50};
+const plotWidth = graphicWidth - margin.left - margin.right;
+const plotHeight = graphicHeight - margin.top - margin.bottom;
+
+// defaults
+const funcText = '3*(x^2)';
+const xMin = 0;
+const xMax = 1;
+
+// plot default
+plotFunction();
+
 // plot function when submitted
 d3.select('#function-submit')
-  .on('click', plotFunction);
+  .on('click', function() {
+      gatherData();
+      plotFunction();
+  });
+
+function gatherData() {
+    funcText = d3.select('#function-input').property('value');
+    xMin = +d3.select('#x-min-input').property('value');
+    xMax = +d3.select('#x-max-input').property('value');
+}
 
 function plotFunction() {
 
     // clear and redraw plot
     // https://stackoverflow.com/questions/14422198/how-do-i-remove-all-children-elements-from-a-node-and-then-apply-them-again-with
-    d3.select("#graphic").selectAll("*").remove();
-    plotSvg = d3.select("#graphic")
-                .append("svg")
-                .attr("width", plotWidth + margin.left + margin.right)
-                .attr("height", plotHeight + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    d3.select('#graphic').selectAll('*').remove();
+    plotSvg = d3.select('#graphic')
+                .append('svg')
+                .attr('width', plotWidth + margin.left + margin.right)
+                .attr('height', plotHeight + margin.top + margin.bottom)
+                .append('g')
+                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
     // convert input text to function
-    const funcText = d3.select('#function-input').property('value');
-    const xMin = d3.select('#x-min-input').property('value');
-    const xMax = d3.select('#x-max-input').property('value');
     const comp = math.compile(funcText);
     function func (val) {
         return comp.evaluate({x: val});
@@ -29,45 +52,11 @@ function plotFunction() {
     const numPoints = 100;
     const scaleFactor = domainSize / numPoints;
     let xVal = xMin;
-    for(let i = 0; i < numPoints; i++){
-        funcData[i] = {x: xVal, y: func(xVal), yInt: math.integrate(func, xMin, xVal)};
+    for(let i = 0; i < numPoints+1; i++){
+        funcData[i] = {x: xVal, y: func(xVal), yInt: math.integrate(func, xMin, xVal, 0.001)};
         xVal += scaleFactor;
     }
     console.log(funcData);
     
-    createDensityPlot(funcData, plotSvg, 'y', 'yInt', plotWidth, plotHeight);
-}
-
-// TODO: replace the functions in drawGraph with this?
-function createDensityPlot(data, plotSvg, xAxisUnits, yAxisUnits, width, height) {
-
-    // add the x Axis
-    const x = d3.scaleLinear()
-                .domain(d3.extent(data.map(d => d[xAxisUnits])))
-                .range([0, width]);
-    plotSvg.append("g")
-           .attr("transform", "translate(0," + height + ")")
-           .call(d3.axisBottom(x));
-
-    // add the y Axis
-    const y = d3.scaleLinear()
-                .domain(d3.extent(data.map(d => d[yAxisUnits])))
-                .range([height, 0]);
-    plotSvg.append("g")
-           .call(d3.axisLeft(y));
-
-    // Plot the curve
-    plotSvg.append("path")
-           .attr("class", "mypath")
-           .datum(data)
-           .attr("fill", "transparent")
-           .attr("opacity", ".8")
-           .attr("stroke", "#000")
-           .attr("stroke-width", 1)
-           .attr("stroke-linejoin", "round")
-           .attr("d",  d3.line()
-                         .curve(d3.curveBasis)
-                         .x(function(d) { return x(d[xAxisUnits]); })
-                         .y(function(d) { return y(d[yAxisUnits]); })
-           );
+    createPlot(funcData, plotSvg, 'y', 'yInt', plotWidth, plotHeight);
 }
